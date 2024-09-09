bf1cdb3a304dbb40559ccb6b39f919cb6907f772
Joseph Francis <josephfrancis100@gmail.com>
2024-09-08 21:23:29 -0500

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Joseph Francis on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
